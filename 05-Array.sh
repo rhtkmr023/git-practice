b05c3d81a80e1f8dda3fcff20213f30ef4dac4ac
rohitkumarn <rhtkmr023@outlook.com>
2024-10-27 22:43:11 +0530

    array

--- 05-Array.sh
+++ 05-Array.sh
@@ -1,9 +1,10 @@
 #!bin/bash
 
-Fruits=("dragon" "pie" "mango" "tamarind")
+Fruits=("dragon" "pie" "mango" "tamarind") #Array
 
 echo "First fruit is: ${fruits[0]}"
 echo "First fruit is: ${fruits[1]}"
 echo "First fruit is: ${fruits[2]}"
+echo "First fruit is: ${fruits[3]}"
 
 echo "First fruit is: ${fruits[@]}"
