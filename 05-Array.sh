529d99eb2216243eea3fce8a55b557a5929ff8a3
rohitkumarn <rhtkmr023@outlook.com>
2024-10-27 22:39:57 +0530

    variables

---
+++ 05-Array.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+Fruits=("dragon" "pie" "mango" "tamarind")
+
+echo "First fruit is: ${fruits[0]}"
+echo "First fruit is: ${fruits[1]}"
+echo "First fruit is: ${fruits[2]}"
+
+echo "First fruit is: ${fruits[@]}"
