9972ce5030eda70a0a640d8241e6ed87ff88ced1
rohitkumarn <rhtkmr023@outlook.com>
2024-10-27 21:54:42 +0530

    added conversation scipt

---
+++ 02.conversation.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+echo "Rohit:: Hi suresh, How r u?"
+echo "Suresh:: Hello Rohit, I am fine, How about u?"
+echo "Rohit:: I am doing fine, thinking about a career switch!"
+echo "Suresh:: All the best, Rohit!"
