05151acc7a191775cce0435da18f6acb1b42433f
rohitkumarn <rhtkmr023@outlook.com>
2024-10-27 22:11:54 +0530

    variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -2,7 +2,7 @@
 X1:Adwik
 X2:Yeshwanth
 
-echo"$X1:: Hi $X2, How r u?"
-echo"$X2:: Hello $X1, I am fine, How about u?"
+echo"$X1:: Hi ${X2}, How r u?"
+echo"${X2}:: Hello $X1, I am fine, How about u?"
 echo"$X1:: I am doing fine, thinking about a career switch!"
-echo"$X2:: All the best, $X1!"
+echo"${X2}:: All the best, $X1!"
