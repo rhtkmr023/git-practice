4c8543d8e5e3a9aee36229890c3a663b4a8df2c3
rohitkumarn <rhtkmr023@outlook.com>
2024-10-27 22:27:39 +0530

    variables

---
+++ 04-variablesA.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+X1=$1
+X2=$2
+
+echo "$X1:: Hi ${X2}, How r u?"
+echo "${X2}:: Hello $X1, I am fine, How about u?"
+echo "$X1:: I am doing fine, thinking about a career switch!"
+echo "${X2}:: All the best, $X1!"
