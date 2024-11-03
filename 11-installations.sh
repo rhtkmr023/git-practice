87de1d241d8ef38069782fa022a46d81c13b94d3
rohitkumarn <rhtkmr023@outlook.com>
2024-11-03 22:11:49 +0530

    functions

--- 11-installations.sh
+++ 11-installations.sh
@@ -8,3 +8,16 @@ then
     echo "Please run this script with root privileges"
     exit 1
 fi
+
+dnf install git
+
+if [ $? -ne 0 ]
+then
+    echo "Git is not installed. Going to install"
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "Git installation not success"
+else
+    echo "Git is already installed, no-installion pending"
+fi
