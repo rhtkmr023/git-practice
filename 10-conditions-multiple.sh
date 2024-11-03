7647f1c5c549c0158d0083683a5fc94cf6e0b30f
rohitkumarn <rhtkmr023@outlook.com>
2024-11-03 21:31:52 +0530

    functions

--- 10-conditions-multiple.sh
+++ 10-conditions-multiple.sh
@@ -5,9 +5,25 @@ NUMBER1=$2
 NUMBER2=$3
 
 
-if [ { $NUMBER0, $NUMBER1, $NUMBER2 } -gt 40 ] #gt, lt, eq, -ne, -le, -ge
+if [ $NUMBER0 -gt 40 ] #gt, lt, eq, -ne, -le, -ge
 then
-    echo "Given number: { $NUMBER0, $NUMBER1, $NUMBER2 } is greater than 40"
+    echo "Given number: $NUMBER0 is greater than 40"
 else
-    echo "Given number: { $NUMBER0, $NUMBER1, $NUMBER2 } is less than 40"
+    echo "Given number: $NUMBER0 is less than 40"
+fi
+
+
+if [ $NUMBER1 -gt 40 ] #gt, lt, eq, -ne, -le, -ge
+then
+    echo "Given number: $NUMBER1 is greater than 40"
+else
+    echo "Given number: $NUMBER1 is less than 40"
+fi
+
+
+if [ $NUMBER2 -gt 40 ] #gt, lt, eq, -ne, -le, -ge
+then
+    echo "Given number: $NUMBER2 is greater than 40"
+else
+    echo "Given number: $NUMBER2 is less than 40"
 fi
