bec8ff2708a80078d2d70cdf320a37efdb60a350
rohitkumarn <rhtkmr023@outlook.com>
2024-11-03 12:25:47 +0530

    functions

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -6,3 +6,5 @@ echo "Script Name: $0"
 echo "current working directory: $PWD"
 echo "Home Directory of Current user: $HOME"
 echo "PID of the script executing now: $$"
+sleep 100 &
+echo "PID of last background command: $!"
