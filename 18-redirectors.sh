7dd5c23a818a6ab3b768b9142b3803aa94997ca4
rohitkumarn <rhtkmr023@outlook.com>
2024-11-10 22:37:01 +0530

    functions

--- 18-redirectors.sh
+++ 18-redirectors.sh
@@ -1,17 +1,22 @@
 #!/bin/bash/
 
 LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
 
 USERID=$(id -u)
 #echo "User ID is: $USERID"
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
 
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-        echo "Please run this script with root privileges"
+        echo -e "$R Please run this script with root privileges $N" $LOG_FILE
         exit 1
     fi
 }
@@ -19,11 +24,36 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is..$R FAILED $N"
+        echo -e "$2 is..$R FAILED $N" $LOG_FILE
         exit 1
     else
-        echo "$2 is..$G SUCCESS $N"
+        echo "$2 is..$G SUCCESS $N" $LOG_FILE
     fi
 }
 
+USAGE(){
+    echo -e "$R USAGE:: $N sudo sh-18-redirectos.sh package1 package2 ..."
+    exit 1
+
+}
+
 CHECK_ROOT
+
+if [ $# -eq 0 ]
+then
+    USAGE
+fi
+
+for package in $@  # $@ is a special varibale where all arguments are passed to it.
+do
+    dnf list installed $package $LOG_FILE
+
+    if [$? - ne 0]
+    then
+        echo " $package is not installed" $LOG_FILE
+        dnf install $package -y $LOG_FILE
+        VALIDATE $? "installing $package" $LOG_FILE
+    else
+        echo -e "$package $Y in already installed..nothing to do $N" $LOG_FILE
+    fi
+done
