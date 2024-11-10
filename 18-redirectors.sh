ecb580d1dad2cab803334339267b394ddf5423ab
rohitkumarn <rhtkmr023@outlook.com>
2024-11-10 22:41:00 +0530

    functions

--- 18-redirectors.sh
+++ 18-redirectors.sh
@@ -16,7 +16,7 @@ Y="\e[33m"
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-        echo -e "$R Please run this script with root privileges $N" $LOG_FILE
+        echo -e "$R Please run this script with root privileges $N" &>>$LOG_FILE
         exit 1
     fi
 }
@@ -24,10 +24,10 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is..$R FAILED $N" $LOG_FILE
+        echo -e "$2 is..$R FAILED $N" &>>$LOG_FILE
         exit 1
     else
-        echo "$2 is..$G SUCCESS $N" $LOG_FILE
+        echo "$2 is..$G SUCCESS $N" &>>$LOG_FILE
     fi
 }
 
@@ -50,10 +50,10 @@ do
 
     if [$? - ne 0]
     then
-        echo " $package is not installed" $LOG_FILE
-        dnf install $package -y $LOG_FILE
-        VALIDATE $? "installing $package" $LOG_FILE
+        echo " $package is not installed" &>>$LOG_FILE
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "installing $package" &>>$LOG_FILE
     else
-        echo -e "$package $Y in already installed..nothing to do $N" $LOG_FILE
+        echo -e "$package $Y in already installed..nothing to do $N" &>>$LOG_FILE
     fi
 done
